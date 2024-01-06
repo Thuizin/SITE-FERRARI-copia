ec6f52e8f308eced4c6b69439dc5ad696ff77176
Matheus M. Prado <102251484+Thuizin@users.noreply.github.com>
2024-01-06 02:01:24 -0300

    removendo código css

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,3 @@
-// LOGO FERRARI APARECER
-let ferrari = document.getElementById("entrada");
-ferrari.style.opacity = "0";
-// window.addEventListener('load', function() {
-//     this.setTimeout(function(){
-//         menu.style.opacity = "1";
-//     }, 3000);
-// });
-
-// for(let x = 0; x <= 10; x++){
-//     ferrari.style.opacity = "1";
-// }
-// for(x = 10; x <= 20; x++){
-//     ferrari.style.opacity = "0";
-// }
-
-
 // MENU APARECER
 let menu = document.getElementById("menu");
 window.addEventListener('load', function() {
