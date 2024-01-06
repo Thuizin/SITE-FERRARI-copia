a8dbb48749dc858c7fa1e8de2f3b73a18b5c4174
Matheus M. Prado <102251484+Thuizin@users.noreply.github.com>
2024-01-06 01:39:47 -0300

    favicon erro

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,19 @@ video1.play();
 
 
 
-
-// MENU APARECER
-let menu = document.getElementById("menu");
-window.addEventListener('load', function() {
-    setTimeout(function() {
-    menu.style.opacity = "1";
-    }, 3000);
-});
-
 // LOGO FERRARI APARECER
 let ferrari = document.getElementById("entrada");
 ferrari.style.opacity = "0";
 window.addEventListener('load', function() {
     this.setTimeout(function(){
         menu.style.opacity = "1";
+    }, 3000);
+});
+
+// MENU APARECER
+let menu = document.getElementById("menu");
+window.addEventListener('load', function() {
+    setTimeout(function() {
+    menu.style.opacity = "1";
     }, 3000);
 });
