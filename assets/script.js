67bfa683a2167479fd4c4adb3833e272f3ef9733
Matheus M. Prado <102251484+Thuizin@users.noreply.github.com>
2024-01-06 01:51:56 -0300

    teste

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,3 @@
-const video1 = document.querySelector("#videos video");
-video1.play();
-
-
-
 // LOGO FERRARI APARECER
 let ferrari = document.getElementById("entrada");
 ferrari.style.opacity = "0";
@@ -11,6 +6,15 @@ ferrari.style.opacity = "0";
 //         menu.style.opacity = "1";
 //     }, 3000);
 // });
+
+for(let x = 0; x <= 10; x++){
+    ferrari.style.opacity = "1";
+}
+for(x = 10; x <= 20; x++){
+    ferrari.style.opacity = "0";
+}
+
+
 // MENU APARECER
 let menu = document.getElementById("menu");
 window.addEventListener('load', function() {
@@ -23,10 +27,3 @@ window.addEventListener('load', function() {
 
 
 
-
-for(let x = 0; x <= 1; x++){
-    ferrari.style.opacity = "1";
-}
-for(x = 1; x <= 3; x++){
-    ferrari.style.opacity = "0";
-}
