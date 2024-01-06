506f50dfe16512b8a81783a0899ab46ee097fa31
Matheus M. Prado <102251484+Thuizin@users.noreply.github.com>
2024-01-06 01:31:46 -0300

    animação entrada ferrari

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,20 @@ video1.play();
 
 
 
+
 // MENU APARECER
 let menu = document.getElementById("menu");
 window.addEventListener('load', function() {
     setTimeout(function() {
     menu.style.opacity = "1";
-    }, 2000);
+    }, 3000);
+});
+
+// LOGO FERRARI APARECER
+let ferrari = document.getElementById("entrada");
+ferrari.style.opacity = "0";
+window.addEventListener('load', function() {
+    this.setTimeout(function(){
+        menu.style.opacity = "1";
+    }, 1000);
 });
