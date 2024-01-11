bc1e882298cd3400bf9141fb2bbd03b28371e158
Matheus M. Prado <102251484+Thuizin@users.noreply.github.com>
2024-01-11 02:40:18 -0300

    alterações 11-01 (02:40)

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,17 @@
 // window.addEventListener('load', function() {
 //     setTimeout(function() {
 //     container.style.opacity = "1";
-//     }, 3000);
+//     }, 2200);
 // });
 
+// ANIMAÇÃO MENU AO SCROLLAR
+window.addEventListener('scroll', function(){
+    let menu = document.getElementById("menu");
+    let scrollThreshold = 0;
+    let scrollPosition = window.scrollY;
+    if(scrollPosition > scrollThreshold){
+        menu.classList.add('scrolled');
+    }else{
+        menu.classList.remove('scrolled');
+    }
+});
