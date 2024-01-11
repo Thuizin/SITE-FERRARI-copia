2720f7488eedf074b490f5091e58fa198d3a324e
Matheus M. Prado <102251484+Thuizin@users.noreply.github.com>
2024-01-11 01:30:00 -0300

    ajustando

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
-// MENU APARECER
-let container = document.getElementById("container");
-window.addEventListener('load', function() {
-    setTimeout(function() {
-    container.style.opacity = "1";
-    }, 3000);
-});
+// // MENU APARECER
+// let container = document.getElementById("container");
+// window.addEventListener('load', function() {
+//     setTimeout(function() {
+//     container.style.opacity = "1";
+//     }, 3000);
+// });
+
